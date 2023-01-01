278b4251a0e66f395eebcc214843e19831f2d174
kangju2000 <kangju2000@naver.com>
2023-01-01 23:52:19 +0900

    feat(SignUp): SignUp page 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from '@/pages/Home/Home';
+import Login from '@/pages/Login/Login';
+import SignUp from '@/pages/SignUp/SignUp';
 import Layout from '@/components/Layout/Layout';
+import ROUTES from '@/constants/routes';
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
+          <Route path={ROUTES.SIGNUP} element={<SignUp />} />
         </Routes>
       </Layout>
     </Router>
---
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,5 @@
+function SignUp() {
+  return <div>SignUp</div>;
+}
+
+export default SignUp;
