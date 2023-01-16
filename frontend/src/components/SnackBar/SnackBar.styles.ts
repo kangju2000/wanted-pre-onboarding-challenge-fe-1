a72997eecbbd349a8ce703dc34d0e75e99b1ec29
kangju2000 <kangju2000@naver.com>
2023-01-16 16:14:36 +0900

    styles: SnackBar 애니메이션 추가

--- frontend/src/components/SnackBar/SnackBar.styles.ts
+++ frontend/src/components/SnackBar/SnackBar.styles.ts
@@ -5,14 +5,37 @@ export const SnackBar = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  bottom: 10px;
+  bottom: 20px;
   left: 50%;
   transform: translateX(-50%);
   width: 300px;
   height: 50px;
   padding: 10px;
-  background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.gray};
   color: ${({ theme }) => theme.colors.white};
   border-radius: 7px;
   z-index: 200;
+  animation: fadein 0.5s, fadeout 0.5s 1.7s;
+
+  @keyframes fadein {
+    from {
+      bottom: 0;
+      opacity: 0;
+    }
+    to {
+      bottom: 20px;
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeout {
+    from {
+      bottom: 20px;
+      opacity: 1;
+    }
+    to {
+      bottom: 0;
+      opacity: 0;
+    }
+  }
 `;
